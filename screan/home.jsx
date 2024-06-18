fd735c0c72fb5cf07699fdf5cc4d0d30ed9b4d22
manjotdevs <manjotdevs459@gmail.com>
2024-06-18 22:52:07 +0530

    battey is added

--- screan/home.jsx
+++ screan/home.jsx
@@ -1,11 +1,27 @@
-import { useBatteryLevel, useBatteryState } from "expo-battery";
+import { useEffect, useState } from "react";
+import { useBatteryLevel } from "expo-battery";
 import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
 import Cr from "../component/circle";
 
+const [batteryLevel, setBatteryLevel] = useState(initialBatteryLevel * 100);
+useEffect(() => {
+  const repeatFunction = async () => {
+    const level = useBatteryLevel() * 100;
+    setBatteryLevel(level);
+  };
+
+  // Set the function to repeat every 60 seconds (60000 milliseconds)
+  const intervalId = setInterval(repeatFunction, 60000);
+  repeatFunction();
+
+  // Clean up the interval when the component is unmounted
+  return () => clearInterval(intervalId);
+}, []);
+
 export default function Home() {
-  const batteryLevel = useBatteryLevel() * 100;
-  const batteryState = useBatteryState();
+  const initialBatteryLevel = useBatteryLevel();
+
   return (
     <>
       <View>
@@ -16,8 +32,11 @@ export default function Home() {
           </Card.Content>
         </Card>
       </View>
-      <Cr progress={5} size={50} strokeWidth={5 } />
-
+      <Cr progress={batteryLevel} size={50} strokeWidth={5} />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  // Add your styles here
+});
